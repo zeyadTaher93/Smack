fdcad0bc9be286fdb6d988ea4e8c88f25213e804
Zeyad Taher <zeyadtaher@zeyads-mac.local>
2019-01-27 07:22:49 -0800

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Zeyad Taher on 1/27/19.
+//  Copyright © 2019 Zeyad Taher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
